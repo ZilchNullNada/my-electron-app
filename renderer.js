e3fff5022878ab3e8d4050ac492996b86d83a256
Samir Bridgmohan <samirbridgmohan99@gmail.com>
2026-05-09 14:39:41 +0930

    made a simple system to load, save, and clear data

--- renderer.js
+++ renderer.js
@@ -1,2 +1,45 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+const inputinfo = {
+    input : document.getElementById("noteInput"),
+    inputDate : document.getElementById("noteDate"),
+    inputTitle : document.getElementById("noteTitle")
+}
+const output = document.getElementById("output");
+
+
+// SAVE
+for (input in inputinfo){
+    inputinfo[input].addEventListener("change", () => {
+        const saveData = {
+            input: inputinfo.input.value,
+            inputDate: inputinfo.inputDate.value,
+            inputTitle: inputinfo.inputTitle.value
+        };
+
+        localStorage.setItem("savedData", JSON.stringify(saveData));
+
+        output.innerText = "Saved!";
+    });
+}
+
+// LOAD
+document.getElementById("loadBtn").addEventListener("click", () => {
+
+    const savedData = localStorage.getItem("savedData");
+
+    const parsedData = JSON.parse(savedData);
+
+    inputinfo.input.value = parsedData.input;
+    inputinfo.inputDate.value = parsedData.inputDate;
+    inputinfo.inputTitle.value = parsedData.inputTitle;
+
+    output.innerText = "Loaded!";
+});
+
+
+// CLEAR
+document.getElementById("clearBtn").addEventListener("click", () => {
+
+    localStorage.removeItem("savedData");
+
+    output.innerText = "Cleared!";
+});
